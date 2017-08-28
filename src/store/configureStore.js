a8830fdcbc7ec4e311df70ce11d6d48dedd69552
Chris Johnson <chris.johnson@madeinhaus.com>
2017-08-28 13:29:00 -0700

    Add `redux-logger` middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import rootReducer from 'reducers';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const logger = createLogger();
+
+const getMiddleware = () => {
+    const middlewares = [thunk];
+
+    if (process.env.NODE_ENV !== 'production') {
+        middlewares.push(logger);
+    }
+
+    return middlewares;
+};
+
+const createStoreWithMiddleware = applyMiddleware(...getMiddleware())(
+    createStore
+);
 
 export default function configureStore(initialState) {
     return createStoreWithMiddleware(rootReducer, initialState);
