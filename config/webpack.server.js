67f49726caae0a7164ea42962b6acc8e23a6993f
Chris Johnson <chris.johnson@madeinhaus.com>
2017-08-28 13:11:01 -0700

    Clean up server file

--- config/webpack.server.js
+++ config/webpack.server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     devtool: 'sourcemap',
@@ -19,10 +18,7 @@ module.exports = {
         modules: [path.join(__dirname, '../src'), 'node_modules'],
         extensions: ['.js', '.jsx', '.json', '.scss'],
     },
-    plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new ExtractTextPlugin('styles.css'),
-    ],
+    plugins: [new webpack.NoEmitOnErrorsPlugin()],
     module: {
         noParse: /(\.min\.js|node_modules)$/,
         rules: [
@@ -32,42 +28,7 @@ module.exports = {
                 include: [path.resolve(__dirname, '../src')],
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'node6', 'stage-0'],
-                },
-            },
-            {
-                test: /\.s?css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        'css-loader',
-                        'sass-loader',
-                        'resolve-url-loader',
-                        'sass-loader?sourceMap',
-                    ],
-                }),
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/,
-                loader: 'url-loader',
-                query: {
-                    limit: 8192,
-                    name: 'images/[name].[ext]?[hash]',
-                },
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader',
-                query: {
-                    limit: 8192,
-                    name: 'fonts/[name].[ext]?[hash]',
-                },
-            },
-            {
-                test: /\.(mp4|avi|mov)$/,
-                loader: 'url-loader',
-                query: {
-                    limit: 10000,
+                    presets: ['node6', 'stage-0'],
                 },
             },
         ],
