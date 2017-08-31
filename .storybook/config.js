783410cd9922e3e2aec7a055c301d6285693f273
Chris Johnson <chris.johnson@madeinhaus.com>
2017-08-31 15:11:28 -0700

    Add React-Router decorator
    Allows us to view Routes/Links/etc from React Router in stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure, addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-router';
 import CenterDecorator from './CenterDecorator';
 
 const req = require.context('../src/components', true, /\.stories\.js$/);
@@ -8,5 +9,6 @@ function loadStories() {
 }
 
 addDecorator(CenterDecorator);
+addDecorator(StoryRouter());
 
 configure(loadStories, module);
