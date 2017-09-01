f060f781adb2a31d75d6f91a73a5d94349d79a15
Chris Johnson <chris.johnson@madeinhaus.com>
2017-09-01 11:28:15 -0700

    Add styles to `List` :facepalm:

--- src/components/molecules/List/List.jsx
+++ src/components/molecules/List/List.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { bool, node } from 'prop-types';
 
+import './List.scss';
+
 const List = ({ children, ordered, ...props }) => {
     if (ordered) {
         return (
-            <ol {...props}>
+            <ol styleName="root" {...props}>
                 {children}
             </ol>
         );
     }
 
     return (
-        <ul {...props}>
+        <ul styleName="root" {...props}>
             {children}
         </ul>
     );
