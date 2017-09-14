55a7d80206a03d33b27401d9a1f404e956f8c8f8
Chris Johnson <chris.johnson@madeinhaus.com>
2017-09-14 12:24:47 -0700

    Convert `CodeSplit` to a functional component and match markup to other example `ecosystems`

--- src/components/ecosystems/CodeSplit/CodeSplit.jsx
+++ src/components/ecosystems/CodeSplit/CodeSplit.jsx
@@ -5,16 +5,17 @@ import { Bundle } from 'common/components';
 const loadMyDep = () =>
     import(/* webpackChunkName: "myDependancy" */ './myDep.js');
 
-export default class CodeSplit extends PureComponent {
-    render() {
-        return (
-            <Bundle load={loadMyDep}>
-                {myDep => (
-                    <h1 onClick={myDep}>
-                        I only render once my dependancy bundle is loaded.
-                    </h1>
-                )}
-            </Bundle>
-        );
-    }
-}
+const CodeSplit = () => (
+    <Bundle load={loadMyDep}>
+        {myDep => (
+            <div>
+                <h1>Code Split</h1>
+                <p onClick={myDep}>
+                    I only render once my dependancy bundle is loaded.
+                </p>
+            </div>
+        )}
+    </Bundle>
+);
+
+export default CodeSplit;
