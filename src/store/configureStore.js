893b1d670449c88ff32843263c48281de7c71efc
Ryan Couse <rynocouse@mac.com>
2017-09-24 21:04:32 -0700

    setup root and reducer HRM

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from 'reducers';
 
 const logger = createLogger();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const getMiddleware = () => {
     const middlewares = [thunk];
@@ -15,10 +16,19 @@ const getMiddleware = () => {
     return middlewares;
 };
 
-const createStoreWithMiddleware = applyMiddleware(...getMiddleware())(
-    createStore
-);
+const createStoreWithMiddleware = composeEnhancers(
+    applyMiddleware(...getMiddleware())
+)(createStore);
 
 export default function configureStore(initialState) {
-    return createStoreWithMiddleware(rootReducer, initialState);
+    const store = createStoreWithMiddleware(rootReducer, initialState);
+    if (module.hot) {
+        // Enable webpack hot module replacement for reducers
+        module.hot.accept('reducers', () => {
+            console.info('Reloading Reducers');
+            const nextReducer = require('reducers/index').default;
+            store.replaceReducer(nextReducer);
+        });
+    }
+    return store;
 }
