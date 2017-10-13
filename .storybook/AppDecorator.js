0c4becc1ae352b1be9bcc7f513b1a34e6582e947
Chris Johnson <chris.johnson@madeinhaus.com>
2017-10-13 14:08:37 -0700

    Change `CenterDecorator` to `AppDecorator`. Add Redux and app class

---
+++ .storybook/AppDecorator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from '../src/store/configureStore';
+
+const store = configureStore();
+
+const AppDecorator = storyFn => {
+    return (
+        <Provider store={store}>
+            <div className="app">{storyFn()}</div>
+        </Provider>
+    );
+};
+
+export default AppDecorator;
