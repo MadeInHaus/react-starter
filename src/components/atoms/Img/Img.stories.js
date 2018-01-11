22854f40928ecf32181b98c7b9f41784a6f0c239
Chris Johnson <chris.johnson@madeinhaus.com>
2018-01-11 13:36:48 -0800

    Run `npm format`

--- src/components/atoms/Img/Img.stories.js
+++ src/components/atoms/Img/Img.stories.js
@@ -5,12 +5,12 @@ import { Img, ImgPreload } from 'components';
 
 storiesOf('Global/Img', module)
     .add('default', () => <Img src="https://placeimg.com/640/480/animals" />)
-    .add('preload', () =>
+    .add('preload', () => (
         <ImgPreload src="https://placeimg.com/1000/1000/any" />
-    )
-    .add('preload (custom loader)', () =>
+    ))
+    .add('preload (custom loader)', () => (
         <ImgPreload
             src="https://placeimg.com/1000/1000/any"
             loader={<div>Loading!</div>}
         />
-    );
+    ));
--- src/components/atoms/Input/Input.stories.js
+++ src/components/atoms/Input/Input.stories.js
@@ -8,13 +8,13 @@ storiesOf('Global/Input', module)
     .add('button (Button)', () => <Input type="button">Click Me</Input>)
     .add('checkbox', () => <Input type="checkbox" />)
     .add('radio', () => <Input type="radio" />)
-    .add('select', () =>
+    .add('select', () => (
         <Input type="select">
             <option>Example 1</option>
             <option>Example 2</option>
             <option>Example 3</option>
             <option>Example 4</option>
         </Input>
-    )
+    ))
     .add('submit (Button)', () => <Input type="submit">Submit</Input>)
     .add('textarea', () => <Input type="textarea" />);
