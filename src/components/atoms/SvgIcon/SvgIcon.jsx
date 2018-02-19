b524bf8096eee772ce611cdabe59e9629e9a017b
m-rstewart <mikestewart90@gmail.com>
2018-02-18 23:03:59 -0800

    SvgIcon can now accept another element as a prop or will default to `span`. #23

--- src/components/atoms/SvgIcon/SvgIcon.jsx
+++ src/components/atoms/SvgIcon/SvgIcon.jsx
@@ -4,15 +4,17 @@ import './SvgIcon.scss';
 
 import icons from './icons';
 
-const SvgIcon = ({ iconType }) => (
-    <span
-        className="icon"
-        dangerouslySetInnerHTML={{ __html: icons[iconType] }}
-    />
-);
+const SvgIcon = ({ iconType, tag }) => {
+    const elementTag = tag || 'span';
+    return React.createElement(`${elementTag}`, {
+        className: 'icon',
+        dangerouslySetInnerHTML: { __html: icons[iconType] },
+    });
+};
 
 SvgIcon.propTypes = {
     iconType: string,
+    tag: string,
 };
 
 export default SvgIcon;
