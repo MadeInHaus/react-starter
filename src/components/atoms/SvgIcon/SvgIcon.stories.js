44b887a5aadf1653fb4d5f6612073b3261340c39
m-rstewart <mikestewart90@gmail.com>
2018-02-20 13:44:53 -0800

    Provide common social media icons as examples for storybook. #23

--- src/components/atoms/SvgIcon/SvgIcon.stories.js
+++ src/components/atoms/SvgIcon/SvgIcon.stories.js
@@ -3,4 +3,19 @@ import { storiesOf } from '@storybook/react';
 
 import { SvgIcon } from 'components';
 
-storiesOf('Global/SvgIcon', module).add('default', () => <SvgIcon />);
+storiesOf('Global/SvgIcon', module).add('default', () => (
+    <div>
+        <div style={{ height: '250px', width: '250px' }}>
+            <SvgIcon iconType="facebook" tag="div" />
+        </div>
+        <div style={{ height: '250px', width: '250px' }}>
+            <SvgIcon iconType="instagram" tag="div" />
+        </div>
+        <div style={{ height: '250px', width: '250px' }}>
+            <SvgIcon iconType="linkedin" tag="div" />
+        </div>
+        <div style={{ height: '250px', width: '250px' }}>
+            <SvgIcon iconType="twitter" tag="div" />
+        </div>
+    </div>
+));
