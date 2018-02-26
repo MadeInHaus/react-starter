2d39e50898c78d871e0ae8da1f05b464572447c8
Michael Stewart <mikestewart90@gmail.com>
2018-02-25 22:04:31 -0800

    Create `SvgIcon` component (#24)
    * Basic files and file structure for `SvgIcon`. #23
    * SvgIcon can now accept another element as a prop or will default to `span`. #23
    * Use defaultProps to set default tag to span. #23
    * Apply correct styles to svg wrapper. #23
    * Provide common social media icons as examples for storybook. #23
    * Turn tag back into a const and make iconType a required prop. #23

---
+++ src/components/atoms/SvgIcon/SvgIcon.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { string } from 'prop-types';
+import styles from './SvgIcon.scss';
+
+import icons from './icons';
+
+const SvgIcon = ({ iconType, tag }) => {
+    return React.createElement(tag, {
+        className: styles.root,
+        dangerouslySetInnerHTML: { __html: icons[iconType] },
+    });
+};
+
+SvgIcon.defaultProps = {
+    tag: 'span',
+};
+
+SvgIcon.propTypes = {
+    iconType: string.isRequired,
+    tag: string,
+};
+
+export default SvgIcon;
