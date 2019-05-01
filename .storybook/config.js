89dc3dc7054d240f305f7fb3737b1596bbd95c79
Clay Cooper <clay@madeinhaus.com>
2019-05-01 16:30:04 -0700

    search for stories throughout entire src

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,13 +2,11 @@ import { configure, addDecorator } from '@storybook/react';
 import StoryRouter from 'storybook-router';
 import AppDecorator from './AppDecorator';
 
-const req = require.context('../src/components', true, /\.stories\.js$/);
-
+// automatically import all files ending in *.stories.js
+const req = require.context('../src', true, /\.stories\.js$/);
 function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
-
 addDecorator(AppDecorator);
 addDecorator(StoryRouter());
-
 configure(loadStories, module);
