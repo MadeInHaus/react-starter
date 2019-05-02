70f27f17d70cfeb54832c283ec5e69fd26713c0a
Clay Cooper <clay@madeinhaus.com>
2019-05-02 11:01:33 -0700

    add knobs example to Button

--- src/common/ui/Button/Button.stories.js
+++ src/common/ui/Button/Button.stories.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { text, boolean, number } from '@storybook/addon-knobs';
 
 import { Button } from 'ui';
 
 storiesOf('Global/Button', module)
     .add('default', () => (
-        <Button onClick={action('Button Clicked')}>Example</Button>
+        <Button
+            onClick={action('Button Clicked')}
+            disabled={boolean('Disabled', false)}
+        >
+            {text('Label', 'Example')} {number('Number', 89)}
+        </Button>
     ))
     .add('primary', () => (
         <Button onClick={action('Button Clicked')} primary>
