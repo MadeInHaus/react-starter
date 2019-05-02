c5ec5e15c6654c86ba3d68f506e7d48340694b5d
Clay Cooper <clay@madeinhaus.com>
2019-05-02 11:01:41 -0700

    add knobs globally

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,14 @@
 import { configure, addDecorator } from '@storybook/react';
 import StoryRouter from 'storybook-router';
 import AppDecorator from './AppDecorator';
+import { withKnobs } from '@storybook/addon-knobs';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /\.stories\.js$/);
 function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
+addDecorator(withKnobs);
 addDecorator(AppDecorator);
 addDecorator(StoryRouter());
 configure(loadStories, module);
