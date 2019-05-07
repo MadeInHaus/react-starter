85a7355e34289b232d9242d7de6df970b995ec5c
Clay Cooper <clay@madeinhaus.com>
2019-05-06 18:25:45 -0700

    register and configure parameters for story viewport

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import StoryRouter from 'storybook-router';
 import AppDecorator from './AppDecorator';
 import { withKnobs } from '@storybook/addon-knobs';
@@ -8,6 +8,33 @@ const req = require.context('../src', true, /\.stories\.js$/);
 function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
+addParameters({
+    viewport: {
+        viewports: {
+            small: {
+                name: 'small',
+                styles: {
+                    width: '375px',
+                    height: '550px',
+                },
+            },
+            medium: {
+                name: 'medium',
+                styles: {
+                    width: '768px',
+                    height: '950px',
+                },
+            },
+            large: {
+                name: 'large',
+                styles: {
+                    width: '1440px',
+                    height: '800px',
+                },
+            },
+        },
+    },
+});
 addDecorator(withKnobs);
 addDecorator(AppDecorator);
 addDecorator(StoryRouter());
