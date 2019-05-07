66c07f1a5f191001497d14e2162eff0c2fcc1d85
Clay Cooper <clay@madeinhaus.com>
2019-05-06 18:27:01 -0700

    use puppeteer to screenshot different sizes

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -1,3 +1,35 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import pupDevices from 'puppeteer/DeviceDescriptors';
 
-initStoryshots();
+const storybookUrl = 'http://localhost:9001';
+
+function createCustomizePage(pupDevice) {
+    return function(page) {
+        return page.emulate(pupDevice);
+    };
+}
+
+if (process.env.NODE_ENV === 'test') {
+    initStoryshots();
+} else if (process.env.NODE_ENV === 'screenshot') {
+    const supportedDevices = new Set(['iPhone 8', 'iPad', 'iPad Pro']);
+
+    for (const supportedDevice of supportedDevices) {
+        const pupDevice = pupDevices[supportedDevice];
+
+        if (!pupDevice) {
+            continue;
+        }
+
+        const customizePage = createCustomizePage(pupDevice);
+
+        initStoryshots({
+            suite: `Image storyshots: ${pupDevice.name}`,
+            test: imageSnapshot({
+                storybookUrl,
+                customizePage,
+            }),
+        });
+    }
+}
