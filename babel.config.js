8d0fc41a1c92789160eda65c49166e9cc1cc4bba
Clay Cooper <clay@madeinhaus.com>
2019-05-06 18:25:24 -0700

    use context-hook for screenshot env

--- babel.config.js
+++ babel.config.js
@@ -32,6 +32,9 @@ module.exports = function(api) {
             test: {
                 plugins: ['require-context-hook'],
             },
+            screenshot: {
+                plugins: ['require-context-hook'],
+            },
         },
     };
 };
