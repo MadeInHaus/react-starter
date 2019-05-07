78d7bdbf83bdf42c2f99dc02f9e47dddb05c1c36
Clay Cooper <clay@madeinhaus.com>
2019-05-06 18:26:33 -0700

    add viewport stories for Button, change background color for testing

--- src/common/ui/Button/Button.stories.js
+++ src/common/ui/Button/Button.stories.js
@@ -6,6 +6,39 @@ import { text, boolean, number } from '@storybook/addon-knobs';
 import { Button } from 'ui';
 
 storiesOf('Global/Button', module)
+    .addParameters({ viewport: { defaultViewport: 'iphone6' } })
+    .add(
+        'with viewport small',
+        () => (
+            <Button
+                onClick={action('Button Clicked')}
+                disabled={boolean('Disabled', false)}
+            >
+                {text('Label', 'Example')} {number('Number', 89)}
+            </Button>
+        ),
+        { viewport: { defaultViewport: 'small' } }
+    )
+    .add(
+        'with viewport large',
+        () => (
+            <Button
+                onClick={action('Button Clicked')}
+                disabled={boolean('Disabled', false)}
+            >
+                {text('Label', 'Example')} {number('Number', 89)}
+            </Button>
+        ),
+        { viewport: { defaultViewport: 'large' } }
+    )
+    .add('default', () => (
+        <Button
+            onClick={action('Button Clicked')}
+            disabled={boolean('Disabled', false)}
+        >
+            {text('Label', 'Example')} {number('Number', 89)}
+        </Button>
+    ))
     .add('default', () => (
         <Button
             onClick={action('Button Clicked')}
@@ -15,7 +48,7 @@ storiesOf('Global/Button', module)
         </Button>
     ))
     .add('primary', () => (
-        <Button onClick={action('Button Clicked')} primary>
+        <Button onClick={action('Button Clicked')} primary="true">
             Example
         </Button>
     ))
