1d7594bbf67907baf6e734280665fc9b67bb9cbf
Clay Cooper <clay@madeinhaus.com>
2019-05-09 17:54:25 -0700

    add Clickable with styles

---
+++ src/common/ui/Clickable/Clickable.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'ui';
+import cx from 'classnames';
+
+import styles from './Clickable.scss';
+
+const Clickable = ({ className, href, theme, to, ...props }) => {
+    const classNames = cx(styles.root, className, styles[theme]);
+
+    if (to || href) {
+        return (
+            <Link {...props} className={classNames} to={to} href={href}>
+                <span>{props.children}</span>
+            </Link>
+        );
+    }
+    return (
+        <button {...props} className={classNames}>
+            <span>{props.children}</span>
+        </button>
+    );
+};
+
+Clickable.propTypes = {
+    children: PropTypes.Node,
+    className: PropTypes.string,
+    href: PropTypes.string,
+    theme: PropTypes.oneOf(Object.keys(styles)),
+    to: PropTypes.string,
+};
+
+Clickable.defaultProps = {
+    className: null,
+    href: null,
+    theme: 'primary',
+    to: null,
+};
+
+export default Clickable;
---
+++ src/common/ui/Clickable/Clickable.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { text, boolean } from '@storybook/addon-knobs';
+
+import { Clickable } from 'ui';
+
+storiesOf('Global/Clickable--SS', module)
+    .add('default', () => (
+        <Clickable
+            onClick={action('Clickable Clicked')}
+            disabled={boolean('Disabled', false)}
+        >
+            {text('Label', 'Example')}
+        </Clickable>
+    ))
+    .add('secondary', () => (
+        <Clickable
+            onClick={action('Clickable Clicked')}
+            theme="secondary"
+            disabled={boolean('Disabled', false)}
+        >
+            Example
+        </Clickable>
+    ))
+    .add('Link (href)', () => (
+        <Clickable href="http://www.google.com">Google</Clickable>
+    ))
+    .add('Link (router)', () => <Clickable to="/">Router Link</Clickable>);
