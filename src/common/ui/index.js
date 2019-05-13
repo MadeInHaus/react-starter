5f6a49cdacaf122ab96a6862f950c077d9c7a6a8
Clay Cooper <clay@madeinhaus.com>
2019-05-13 11:30:03 -0700

    fix ImgPreload import

--- src/common/ui/index.js
+++ src/common/ui/index.js
@@ -1,6 +1,6 @@
-export { default as Button } from './Button';
 export { default as Clickable } from './Clickable';
 export { default as Img } from './Img';
+export { default as ImgPreload } from './Img/ImgPreload';
 export { default as Input } from './Input';
 export { default as Link } from './Link';
 export { default as Loader } from './Loader';
