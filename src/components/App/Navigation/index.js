c52db664afc17ff9953bde515d3174b9b2d5f50d
Clay Cooper <clay@madeinhaus.com>
2019-05-13 14:37:47 -0700

    replace Links with Clickable

--- src/components/App/Navigation/index.js
+++ src/components/App/Navigation/index.js
@@ -1,44 +1,46 @@
 import React from 'react';
-import { Button, SvgIcon } from '@ui';
+import { Clickable, SvgIcon } from '@ui';
 
 import styles from './Navigation.scss';
 
 const Navigation = () => (
     <nav className={styles.root}>
         <span>
-            <Button
+            <Clickable
                 exact
                 to="/"
                 activeClassName={styles.selected}
                 className={styles.logo}
             >
                 // SPA Starter
-            </Button>
+            </Clickable>
         </span>
         <span>
-            <Button
+            <Clickable
                 to="/code-split"
                 theme="body"
                 activeClassName={styles.selected}
                 className={styles.link}
             >
                 Code Splitting
-            </Button>
-            <Button
+            </Clickable>
+            <Clickable
                 to="/makejavascriptgreatagain"
+                theme="body"
                 activeClassName={styles.selected}
                 className={styles.link}
             >
                 404
-            </Button>
-            <Button
+            </Clickable>
+            <Clickable
                 href="https://github.com/MadeInHaus/react-redux-webpack-starter"
                 target="_blank"
                 rel="noopener noreferrer"
+                theme="body"
                 className={styles.link}
             >
                 <SvgIcon iconType="github" />
-            </Button>
+            </Clickable>
         </span>
     </nav>
 );
