10650d1091543e4b257ff4cf8044d28a51138ed2
Clay Cooper <clay@madeinhaus.com>
2019-05-14 16:04:18 -0700

    wip unit tests

---
+++ src/components/App/App.test.js
@@ -0,0 +1,11 @@
+describe('Addition', () => {
+    it('knows that 2 and 2 make 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
+
+describe('Addition', () => {
+    it('knows that 2 and 2 make 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
